9ede4d861d0236b50519d5ed4a66d4adf396bfd8
Michael Vinh Xuan Thanh <meta.orphic@gmail.com>
2018-01-07 14:25:35 -0700

    Added custom functions to Linux file, set a few items in darwin file, abstracted custom functions from install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -13,49 +13,20 @@ DIST=$(uname)
 if [[ "$DIST" == "Darwin" ]]; then
     source Darwinfile
     # TODO: install xcode
-    # TODO: install brew
-    # TODO: use brew and Brewfile, Caskfile to install apps
-else
+elif [[ "$DIST" == "Linux" ]]; then
     source Linuxfile
     # TODO: Differentiate between debian and ubuntu, apt-get vs yum
-    # TODO: Install all packages in packages array
     for p in $PACKAGES; do
         sudo apt-get install -y $p
         echo 'Installed ' $p
     done
+    for f in $FUNCTIONS; do
+        $f
+    done
+else
+    echo 'note: distribution ${DIST} not currently supported'
+    exit 0
 fi
 
 
-# # Auto-load redshift on startup
-# sudo echo << EOF > /etc/init.d/redshift.sh
-# #!/bin/sh
-# echo "Starting redshift"
-# redshift
-# EOF
-
-# sudo chmod +x /etc/init.d/redshift.sh
-# sudo update-rc.d /etc/init.d/redshift.sh defaults 100
-
-################################
-## Installs fuzzy file finder ##
-################################
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-############################
-## Installs emacs plugins ##
-############################
-git submodule init
-cd .emacs.d/plugins/helm && make
-
-# ##################################
-# ## Miscellaneous Administrative ##
-# ##################################
-# # Disables guest sessions
-# sudo sh -c "echo 'allow-guest=false' >> /usr/share/lightdm/lightdm.conf.d/50-ubuntu.conf"
-
-# # Show name in top right panel
-# gsettings set com.canonical.indicator.session show-real-name-on-panel true
-
-# # One-click minimize on tray icons
-# gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+# TODO: Symlink config files in env-generator w/ home directory
