0c84587631a46b530014c33d5577f2b5ced1dfb7
Vinny Thanh <vthanh@firehydrant.io>
2022-03-15 14:23:07 -0600

    Update NVM installation

--- install_packages.sh
+++ install_packages.sh
@@ -32,3 +32,12 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 
 # Creates symlinks to dotfiles and other files in home directory
 source ./create_links.sh
+
+# Installs NVM and adds NVM hook into .zshrc
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+cat > ~/.zshrc <<- EOF
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+EOF
